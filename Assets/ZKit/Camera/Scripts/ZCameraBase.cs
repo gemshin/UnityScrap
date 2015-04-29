77f3d52a7984ca1abe8bddba5cb018e5fd987d91
gemshin <gemshin@gmail.com>
2015-04-30 00:08:18 +0900

    카메라 추가.

---
+++ Assets/ZKit/Camera/Scripts/ZCameraBase.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZCameraBase : MonoBehaviour
+{
+    protected Transform m_Cam; // the transform of the camera
+
+    protected virtual void Awake()
+    {
+        // find the camera in the object hierarchy
+        m_Cam = GetComponentInChildren<Camera>().transform;
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
